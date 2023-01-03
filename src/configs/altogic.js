696aca2750d2633362454e12d580b1c6c21b6d89
BLI-P <94859315+BLI-P@users.noreply.github.com>
2023-01-03 16:13:36 +0530

    Update altogic.js

--- src/configs/altogic.js
+++ src/configs/altogic.js
@@ -1,8 +1,8 @@
 import { createClient } from "altogic";
 
 // This `envUrl` and `clientKey` is sample you need to create your own.
-const envUrl = "https://ironhide.c1-europe.altogic.com";
-export const clientKey = "2c8df488d7c44a879ebe85027ca9c87d";
+const envUrl = "https://online-shopping-list.c1-europe.altogic.com";
+export const clientKey = "419ab1e3b2f14e44bd7c8be9f08468d4";
 
 const altogic = createClient(envUrl, clientKey, {
   // signInRedirect: "/sign-in",
